3868c25945b1afe9c1621ab96e7ea800f4543dc8
Keerthi-Sivakumar <69709767+Keerthi-Sivakumar@users.noreply.github.com>
2020-08-15 15:21:56 +0530

    php

---
+++ index.php
@@ -0,0 +1,265 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        name="viewport">
+  <link href="https://fonts.googleapis.com/css2?family=Acme&display=swap"
+        rel="stylesheet">
+  <link crossorigin="anonymous"
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+        rel="stylesheet">
+  <link href="css/style.css"
+        rel="stylesheet">
+  <script crossorigin="anonymous"
+        src="https://kit.fontawesome.com/64d58efce2.js"></script>
+  <title>Hilife.ai</title>
+</head>
+<body>
+  <!-- Just an image -->
+  <header class="ims">
+    <nav class="navbar"
+         style="height: 80px;background-color:rgb(8, 133, 150) ;background-position: center; background-size: cover;">
+          
+      <a class="navbar-brand"
+           href="#"
+           style="font-family:'Acme',sans-serif;font-size: 35px; color: #fff;"><img alt=""
+           height="70"
+           loading="lazy"
+           src="Logo.png"
+           width="80"> IMS</a>
+      <div class="btn-group">
+        <button class="btn btn-secondary dropdown-toggle"
+             data-toggle="dropdown"
+             id="dropsecondary"
+             style="margin-top: -45px; display:block; font-size: smaller;margin-right: 105px;width: 300px;text-align: start;color: black;background-color:#fff;height: 30px;"
+             type="button">Quick Links</button>
+        <div class="dropdown-menu dropdown-menu" style="font-size: smaller;">
+          <button class="dropdown-item"
+               type="button" style="color: rgb(8, 133, 150);">Attendance</button> <button class="dropdown-item"
+               type="button" style="color: rgb(8, 133, 150);">Final Year Report</button> <button class="dropdown-item"
+               type="button" style="color: rgb(8, 133, 150);">Mark Allocation</button><button class="dropdown-item"
+               type="button" style="color: rgb(8, 133, 150);">Appraisal Notice</button> <button class="dropdown-item"
+               type="button" style="color: rgb(8, 133, 150);">Internal Mark Report</button><button class="dropdown-item"
+               type="button" style="color: rgb(8, 133, 150);">R&D Announcement</button>
+        </div>
+      </div>
+    </nav>
+    <div class="container">
+     
+   
+      <div class="nav-btn">
+        <div class="nav-links">
+          <ul>
+            <li class="nav-link"
+                style="--i: 0.60s">
+              <a href="#">Home</a>
+            </li>
+            <li class="nav-link"
+                style="--i: .6s">
+              <a href="#">IMS Navigation</a>
+              <div class="dropdown">
+                <ul>
+                  <li class="dropdown-link">
+                    <a href="#">Division Of Administration</a>
+                  </li>
+                  <li class="dropdown-link">
+                    <a href="#">Division Of LMS</a>
+                  </li>
+                  <li class="dropdown-link">
+                    <a href="#">Link 3</a>
+                    <div class="dropdown second">
+                      <ul>
+                        <li class="dropdown-link">
+                          <a href="#">Link 1</a>
+                        </li>
+                        <li class="dropdown-link">
+                          <a href="#">Link 2</a>
+                        </li>
+                        <li class="dropdown-link">
+                          <a href="#">Link 3</a>
+                        </li>
+                        <li class="dropdown-link">
+                          <a href="#">More</a>
+                          <div class="dropdown second">
+                            <ul>
+                              <li class="dropdown-link">
+                                <a href="#">Link 1</a>
+                              </li>
+                              <li class="dropdown-link">
+                                <a href="#">Link 2</a>
+                              </li>
+                              <li class="dropdown-link">
+                                <a href="#">Link 3</a>
+                              </li>
+                              <div class="arrow"></div>
+                            </ul>
+                          </div>
+                        </li>
+                        <div class="arrow"></div>
+                      </ul>
+                    </div>
+                  </li>
+                  <li class="dropdown-link">
+                    <a href="#">Link 4</a>
+                  </li>
+                  <div class="arrow"></div>
+                </ul>
+              </div>
+            </li>
+            <li class="nav-link"
+                style="--i: 1.1s">
+              <a href="#">Change Password</a>
+            </li>
+            <li class="nav-link"
+                style="--i: 1.35s">
+              <a href="#">e-Resource Creation</a>
+            </li>
+            <li class="nav-link"
+                style="--i: 1.60s">
+              <a href="#">Live</a>
+            </li>
+          </ul>
+        </div>
+        <div class="log-sign"
+             style="--i: 1.8s">
+          <a href="#" style="text-decoration: none;color: rgb(8, 133, 150);font-size: smaller;"><i class="fa fa-undo" aria-hidden="true"></i>&nbsp; Go to Hilife &emsp;</a>
+        </div>
+        <div class="log-sign"
+             style="--i: 1.8s">
+          <a 
+               href="#" style="text-decoration: none;color: rgb(8, 133, 150);font-size: smaller;"><i class="fa fa-question-circle" aria-hidden="true"></i>&nbsp; Help &emsp;</a>
+        </div>
+        <div class="log-sign"
+             style="--i: 1.8s">
+          <a 
+               href="index.php" style="text-decoration: none;color: rgb(8, 133, 150);font-size: smaller;"><i class="fa fa-sign-out" aria-hidden="true"></i>&nbsp; Log Out</a><br>
+        </div>
+      </div>
+    </div>
+  </header>
+  <div class="content">
+    <div class="sidebar">
+      <center>
+        <img alt=" "
+                class="profile_image"
+                src="customer.png">
+        <h4>Hilife</h4>
+      </center><a href="#"><span>Inbox</span></a> <a href="#"><span>Library Search</span></a> <a href="#"><span>Library book issued</span></a> <a href="att_form.php"><span>Attendance</span></a> <a href="#"><span>Mark Allocation</span></a> <a href=
+      "announcetask"><span>Announcement</span></a>
+    </div>
+    <div class="container">
+      <div class="sub-content">
+        <nav class="navbar navbar-light">
+          <a class="navbar-brand"></a>
+          <form class="form-inline right">
+            <input aria-label="Search"
+                  class="form-control mr-sm-2"
+                  placeholder="Search"
+                  type="search"> <button class="btn btn-outline-success my-2 my-sm-0"
+                  type="submit"  style="margin-left: -10px;height: 38px;">Go</button>
+          </form>
+        </nav>
+        <div class="notification" >
+          <ul class="nav nav-tabs"
+              id="myTab"
+              role="tablist">
+            <li class="nav-item"
+                role="presentation">
+              <a 
+                  class="nav-link active"
+                  data-toggle="tab"
+                  href="#Task"
+                  id="Task-tab"
+                  role="tab"
+                  style="color: black;font-size: 15px;">Task</a>
+            </li>
+            <li class="nav-item"
+                role="presentation">
+              <a 
+                  class="nav-link"
+                  data-toggle="tab"
+                  href="#Activities"
+                  id="Activities-tab"
+                  role="tab"
+                  style="color: black;font-size: 15px;">Activities</a>
+            </li>
+            <li class="nav-item"
+                role="presentation">
+              <a 
+                  class="nav-link"
+                  data-toggle="tab"
+                  href="#Message"
+                  id="Message-tab"
+                  role="tab"
+                  style="color: black;font-size: 15px;">Message</a>
+            </li>
+          </ul>
+          <div class="tab-content"
+               id="myTabContent">
+            <div aria-labelledby="Task-tab"
+                 class="tab-pane fade show active"
+                 id="Task"
+                 role="tabpanel"
+                 style="text-align: center;">
+                 
+              Task will be added
+            </div>
+            <div aria-labelledby="Activities-tab"
+                 class="tab-pane fade"
+                 id="Activities"
+                 role="tabpanel"
+                 style="text-align: center;">
+              Activities will be added
+            </div>
+            <div aria-labelledby="Message-tab"
+                 class="tab-pane fade"
+                 id="Message"
+                 role="tabpanel"
+                 style="text-align: center;">
+              Message will be added
+            </div>
+          </div>
+        </div><br>
+        <button class="btn btn-outline-primary"
+             style="color: #353535;text-align:center"
+             type="submit">Notification <span class="badge badge-danger"
+              id="noti_num"></span></button> <button class="btn btn-outline-primary"
+             type="submit"><a href="Event.php"
+           style="color: #353535;">Event</a></button>
+      </div>
+    </div>
+  </div>
+  <script crossorigin="anonymous"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script> 
+  <script crossorigin="anonymous"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script> 
+  <script crossorigin="anonymous"
+        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
+        src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script> 
+  <script type="text/javascript">
+
+        function loadDoc() {
+            setInterval(function() {
+
+                var xhttp = new XMLHttpRequest();
+                xhttp.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+                        document.getElementById("noti_num ").innerHTML = this.responseText;
+                    }
+                };
+                xhttp.open("GET ", "notification.php ", true);
+                xhttp.send();
+
+
+
+            }, 1000);
+
+        }
+        loadDoc();
+  </script>
+</body>
+</html>
